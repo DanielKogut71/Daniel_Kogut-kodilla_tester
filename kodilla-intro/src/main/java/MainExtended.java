ee0e2a4c16ade31f44022cdb875f59f49516ef24
DanielKogut71 <danielkogut710@gmail.com>
2021-02-07 21:47:05 +0100

    Variables

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,27 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Damian";
+        int wiek = 25 ;
+        double height = 1.88;
+        boolean isGoodTester = true ;
+        boolean ownerCar = true;
+        String kindCar = "Fiat126p";
+        char rate = 'A';
+
+
+        System.out.println("Name:");
+        System.out.println(name);
+        System.out.println("Age:");
+        System.out.println(wiek);
+        System.out.println("Height");
+        System.out.println(height);
+        System.out.println("Is a good tester?");
+        System.out.println(isGoodTester);
+        System.out.println("Owner Car?");
+        System.out.println(ownerCar);
+        System.out.println("Kind Car:");
+        System.out.println(kindCar);
+        System.out.println("Rate:");
+        System.out.println(rate);
+    }
+}
